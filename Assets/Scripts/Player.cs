913309680777898bc38eb19e8ad9d7e66982ae4d
Juan Londoño <57278226+juancamilo32@users.noreply.github.com>
2023-02-08 09:58:22 -0500

    Score and death UI added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     float horizontalInput = 0f;
     float xLimit = 3.1f;
+    float highestY = 0;
+    int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,33 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Movement();
+        UpdateScore();
+        ManageDeath();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = horizontalInput;
+        rb.velocity = velocity;
+    }
+
+    void UpdateScore()
+    {
+        if (transform.position.y > highestY)
+        {
+            highestY = transform.position.y;
+            score = Mathf.FloorToInt(highestY * 5);
+        }
+        if (transform.position.y > 6)
+        {
+            UIManager.Instance.UpdateScore(score);
+        }
+    }
+
+    void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
         if (transform.position.x >= xLimit)
@@ -31,10 +60,15 @@ public class Player : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    void ManageDeath()
     {
-        Vector2 velocity = rb.velocity;
-        velocity.x = horizontalInput;
-        rb.velocity = velocity;
+        if (transform.position.y < (highestY - 7f))
+        {
+            UIManager.Instance.EnableDeathScreen(score);
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameManager.Instance.RestartGame();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("UI Manager is null.");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+    [SerializeField]
+    TextMeshProUGUI bestScoreText;
+    [SerializeField]
+    GameObject deathScreen;
+
+    private void Start()
+    {
+        scoreText.text = "Score: 0";
+        bestScoreText.text = "Best: 0";
+    }
+
+    public void UpdateScore(int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void UpdateBestScore(int score)
+    {
+        bestScoreText.text = "Best: " + score.ToString();
+    }
+
+    public void EnableDeathScreen(int score)
+    {
+        deathScreen.gameObject.SetActive(true);
+        deathScreen.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "Score: " + score.ToString(); ;
+    }
+
+}
